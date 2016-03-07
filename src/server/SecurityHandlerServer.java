fd64b76b4ba267d1de4d0fba5252fac99e4f9806
Cyril Shelke <Cyril Shelke>
2016-03-07 01:45:13 -0500

    removing sysouts

--- src/server/SecurityHandlerServer.java
+++ src/server/SecurityHandlerServer.java
@@ -1,4 +1,5 @@
 package server;
+
 import java.security.*;
 //import org.apache.commons.codec.binary.Base64;
 import java.util.Base64;
@@ -6,40 +7,35 @@ import java.util.Base64;
 import javax.crypto.*;
 
 public class SecurityHandlerServer {
-	
-	public static Key gen() throws NoSuchAlgorithmException
-	{
+
+	public static Key gen() throws NoSuchAlgorithmException {
 		KeyGenerator kg = KeyGenerator.getInstance("AES");
 		kg.init(128);
 		return kg.generateKey();
-		
+
 	}
-	
+
 	public static String encryptTheMessage(String message, Key key) throws NoSuchAlgorithmException,
-	NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
-	{
+			NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		Cipher cyphr = Cipher.getInstance("AES");
-        cyphr.init(Cipher.ENCRYPT_MODE, key);
-        byte[] byteDataToEncrypt = message.getBytes();
-        byte[] byteCipherText = cyphr.doFinal(byteDataToEncrypt);
-        return Base64.getEncoder().encodeToString(byteCipherText);
+		cyphr.init(Cipher.ENCRYPT_MODE, key);
+		byte[] byteDataToEncrypt = message.getBytes();
+		byte[] byteCipherText = cyphr.doFinal(byteDataToEncrypt);
+		return Base64.getEncoder().encodeToString(byteCipherText);
 	}
-	
-	public static String decryptTheMessage(String msg,Key key) throws NoSuchAlgorithmException, 
-	NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
-	{
+
+	public static String decryptTheMessage(String msg, Key key) throws NoSuchAlgorithmException, NoSuchPaddingException,
+			InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
 		byte[] data = java.util.Base64.getDecoder().decode(msg);
-        Cipher cyphr = Cipher.getInstance("AES");
-        cyphr.init(Cipher.DECRYPT_MODE, key);
-        byte[] plainData = cyphr.doFinal(data);
-        String decryptedMsg = new String(plainData);
+		Cipher cyphr = Cipher.getInstance("AES");
+		cyphr.init(Cipher.DECRYPT_MODE, key);
+		byte[] plainData = cyphr.doFinal(data);
+		String decryptedMsg = new String(plainData);
 
-        return decryptedMsg;
+		return decryptedMsg;
 	}
 
-	
-	public static String keyToString(Key k)
-	{
+	public static String keyToString(Key k) {
 		String key = Base64.getEncoder().encodeToString(k.getEncoded());
 		return key;
 	}
--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -8,120 +8,98 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import java.io.*;
 
-public class ServerThread extends Thread
-{  
-   private Server server    = null;
-   private Socket socket    = null;
-   private int ID = -1;
-   private DataInputStream  streamIn  =  null;
-   private DataOutputStream streamOut = null;
+public class ServerThread extends Thread {
+	private Server server = null;
+	private Socket socket = null;
+	private int ID = -1;
+	private DataInputStream streamIn = null;
+	private DataOutputStream streamOut = null;
 
-   public ServerThread(Server _server, Socket _socket)
-   {  
-	   super();
-      server = _server;
-      socket = _socket;
-      ID = socket.getPort();
-   }
-   
-   private void sendKey(String msg) throws IOException {
-	   streamOut.writeUTF("<= "+msg);
-	   streamOut.flush();
-	
-   }
-   
-   public void send(String msg) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException
-   {   
-	   try
-       {  
-		  System.out.println("sending the message: <= "+msg);
-		  streamOut.writeUTF(SecurityHandlerServer.encryptTheMessage("<= "+msg, server.getKey()));
-          streamOut.flush();
-       }
-       catch(IOException ioe)
-       {  
-    	   System.out.println(ID + " ERROR sending: " + ioe.getMessage());
-          server.remove(ID);
-          stop();
-       }
-   }
-   
-   public int getID()
-   {  
-	   return ID;
-   }
-   
-   public void run() 
-   {  
-	   
-	   try
-       {  
-		   this.sendKey("/key " + SecurityHandlerServer.keyToString(server.getKey()));
-		   while(true)
-		   {	   
-			   int retVal = server.checkForDups(ID,SecurityHandlerServer.decryptTheMessage(streamIn.readUTF(),server.getKey()));
-			   
-			   if(retVal == 0)
-			   {   
-				   this.send("Exceeded the number of clients in the chat server.");
-			   	   this.send("Exiting..");
-			   	   server.handle(ID, "/leave");
-			   	   return;
-			   }
-			   else if(retVal == 1)
-			   {
-				   this.send("Sorry, Name taken");
-				   this.send("Login Name?");
-				   continue;
-			   }
-			   else if (retVal ==2 )
-			   {
-				   this.send("Welcome "+server.getName(ID)+"!");
-				   break;
-			   }
-		   }
-       }catch(IOException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException ioe)
-		       {  
-		      	 System.out.println(ID + " ERROR reading: " + ioe.getMessage());
-		          server.remove(ID);
-		          stop();
-		       }
-	   
-	  
-      while (true)
-      {  
-    	  try
-         {  
-    	  
-			server.handle(ID, SecurityHandlerServer.decryptTheMessage(streamIn.readUTF(),server.getKey()));
-		
-         }catch(IOException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException
- 				| BadPaddingException e){  
-	        	 System.out.println(ID + " ERROR reading: " + e.getMessage());
-	            server.remove(ID);
-	            stop();
-	         	}
-      }
-   }
-   
+	public ServerThread(Server _server, Socket _socket) {
+		super();
+		server = _server;
+		socket = _socket;
+		ID = socket.getPort();
+	}
 
+	private void sendKey(String msg) throws IOException {
+		streamOut.writeUTF("<= " + msg);
+		streamOut.flush();
 
-public void open() throws IOException
-   {  
-	   streamIn = new DataInputStream(new 
-                        BufferedInputStream(socket.getInputStream()));
-      streamOut = new DataOutputStream(new
-                        BufferedOutputStream(socket.getOutputStream()));
-     
-   }
-   
-   public void close() throws IOException
-   {  
-	   if (socket != null)    
-		   socket.close();
-      if (streamIn != null)  
-    	  streamIn.close();
-      if (streamOut != null) 
-    	  streamOut.close();
-   }
+	}
+
+	public void send(String msg) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException,
+			IllegalBlockSizeException, BadPaddingException {
+		try {
+			//System.out.println("sending the message: <= " + msg);
+			streamOut.writeUTF(SecurityHandlerServer.encryptTheMessage("<= " + msg, server.getKey()));
+			streamOut.flush();
+		} catch (IOException ioe) {
+			System.out.println(ID + " ERROR sending: " + ioe.getMessage());
+			server.remove(ID);
+			stop();
+		}
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void run() {
+
+		try {
+			this.sendKey("/key " + SecurityHandlerServer.keyToString(server.getKey()));
+			while (true) {
+				int retVal = server.checkForDups(ID,
+						SecurityHandlerServer.decryptTheMessage(streamIn.readUTF(), server.getKey()));
+
+				if (retVal == 0) {
+					this.send("Exceeded the number of clients in the chat server.");
+					this.send("Exiting..");
+					server.handle(ID, "/leave");
+					return;
+				} else if (retVal == 1) {
+					this.send("Sorry, Name taken");
+					this.send("Login Name?");
+					continue;
+				} else if (retVal == 2) {
+					this.send("Welcome " + server.getName(ID) + "!");
+					break;
+				}
+			}
+		} catch (IOException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException
+				| IllegalBlockSizeException | BadPaddingException ioe) {
+			System.out.println(ID + " ERROR reading: " + ioe.getMessage());
+			server.remove(ID);
+			stop();
+		}
+
+		while (true) {
+			try {
+
+				server.handle(ID, SecurityHandlerServer.decryptTheMessage(streamIn.readUTF(), server.getKey()));
+
+			} catch (IOException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException
+					| IllegalBlockSizeException | BadPaddingException e) {
+				System.out.println(ID + " ERROR reading: " + e.getMessage());
+				server.remove(ID);
+				stop();
+			}
+		}
+	}
+
+	public void open() throws IOException {
+		streamIn = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+		streamOut = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+
+	}
+
+	public void close() throws IOException {
+		if (socket != null)
+			socket.close();
+		if (streamIn != null)
+			streamIn.close();
+		if (streamOut != null)
+			streamOut.close();
+	}
 }
